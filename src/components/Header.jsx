a2347fefb80b6c9f1843396a7c599ece973bb7fa
Matheus <matheus_sousa@protonmail.com>
2022-06-18 15:30:09 -0300

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
+import styles from './Header.module.css'
+
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export function Header() {
     return (
-        <strong>Ignite Feed</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} />
+        </header>
     );
 }
