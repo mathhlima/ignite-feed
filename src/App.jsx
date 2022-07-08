695b81ac4ae0e941f02a35fbd6012b8e6d6a0e63
Matheus <matheus_sousa@protonmail.com>
2022-07-07 22:23:06 -0300

    feat: form and post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+
+import './global.css'
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 function App() {
 
   return (
-    <Header />
+    <div>
+
+      <Header />
+  
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
+    </div>
   )
 }
 
